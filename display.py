e1f8bfe227b5324916f910ed654fe9ca0906e448
Simon Curtis <s.curtis@uq.net.au>
2020-05-05 12:39:44 +1000

    Add terminal display program

---
+++ display.py
@@ -0,0 +1,54 @@
+import csv
+import operator
+
+
+class Netflow:
+   def __init__(self, src_ip, dst_ip, src_port, dst_port, proto, octs, pkts):
+      self.src_ip = src_ip
+      self.dst_ip = dst_ip
+      self.src_port = src_port
+      self.dst_port = dst_port
+      self.proto = proto
+      self.pkts = pkts
+      self.octs = octs
+
+   def toString(self):
+      return self.src_ip.ljust(18)\
+         +self.dst_ip.ljust(18)\
+         +self.src_port.ljust(14)\
+         +self.dst_port.ljust(19)\
+         +self.proto.ljust(11)\
+         +self.pkts.ljust(9)\
+         +self.octs
+
+
+entries = []
+
+def main ():
+   with open ("/tmp/netflow.csv", "r") as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter=',')
+      sorted_csv = sorted(csv_reader, key=operator.itemgetter(5))
+      counter = 0
+      for row in sorted_csv:
+         if counter == 10:
+            break
+         entry = Netflow(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+         entries.append(entry)
+         counter += 1
+
+   header_string = "Source IP".ljust(18)\
+                  +"Destination IP".ljust(18)\
+                  +"Source Port".ljust(14)\
+                  +"Destination Port".ljust(19)\
+                  +"Protocol".ljust(11)\
+                  +"Packets".ljust(9)\
+                  +"Bytes\n"
+
+   print(header_string)
+   for entry in entries:
+      print(entry.toString());
+
+
+
+if __name__ == "__main__":
+   main()
